dda0ffa3752b3b1a3a89cc7008b2a78500581a68
SchlexanderOlz <Ihavenonameforthischannel@gmail.com>
2024-08-25 13:43:32 +0200

    Swaped Game-Server match-creation, with haproxy conf

--- game-server/src/main.rs
+++ game-server/src/main.rs
@@ -1,10 +1,8 @@
-use listener::{MatchCreated, ModeServerMatchCreated};
 use serde::Serialize;
-use tracing::{debug, info};
+use tracing::info;
 use tracing_subscriber::FmtSubscriber;
 
 mod games;
-mod listener;
 
 #[derive(Serialize)]
 pub struct GameServer {
@@ -25,8 +23,6 @@ pub struct GameMode {
 async fn main() {
     tracing::subscriber::set_global_default(FmtSubscriber::default()).unwrap();
     info!("Starting Server");
-    let app = axum::Router::new();
-    let router = listener::listen(app, on_new_match);
 
     let public_addr = std::env::var("PUBLIC_ADDR").expect("PUBLIC_ADDR must be set");
     let server_info = GameServer {
@@ -48,34 +44,4 @@ async fn main() {
         .await
         .unwrap();
     info!("Registered Game at {url}");
-
-    let addr = std::env::var("HOST_ADDR").expect("HOST_ADDR must be set");
-    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
-    axum::serve(listener, router).await.unwrap();
-}
-
-async fn on_new_match(new_match: listener::CreateMatch) -> MatchCreated {
-    // TODO: Dynamically create the match instead of always Schnapsen Duo
-    debug!("Creating new match: {:?}", new_match);
-    let create_url =
-        std::env::var("SCHNAPSEN_DUO_CREATE_URL").expect("SCHNAPSEN_DUO_CREATE_URL must be set");
-    let client = reqwest::Client::new();
-    let response: ModeServerMatchCreated = client
-        .post(create_url.as_str()) // TODO: Get the actuall port of the Mode-Server from some in-memory store
-        .json(&new_match)
-        .send()
-        .await
-        .unwrap()
-        .json()
-        .await
-        .unwrap();
-
-    // TODO: The created match should actually be added to some state-management of active matches. This server is just a simple temporary implementation.
-    let match_addr = std::env::var("SCHNAPSEN_DUO_PUBLIC_ADDR").expect("SCHNAPSEN_DUO_PUBLIC_ADDR must be set");
-
-    MatchCreated {
-        player_write: response.player_write,
-        read: response.read,
-        url: match_addr,
-    }
 }
