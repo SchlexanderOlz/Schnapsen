bbbef781c07da00ee4cfe55fc679a47fbff624c6
SchlexanderOlz <Ihavenonameforthischannel@gmail.com>
2025-01-07 10:43:29 +0100

    Fixed swap_to call structure

--- schnapsen-rs/src/lib.rs
+++ schnapsen-rs/src/lib.rs
@@ -819,6 +819,9 @@ impl SchnapsenDuo {
     }
 
     fn swap_to(&mut self, player: Arc<RwLock<Player>>) {
+        self.update_announcable_props(player.clone());
+        self.update_swap_trump(player.clone());
+        self.update_playable_cards(player.clone());
         if self.active.is_none() || Arc::ptr_eq(&player, self.active.as_ref().unwrap()) {
             return;
         }
@@ -829,9 +832,6 @@ impl SchnapsenDuo {
         });
 
         self.make_active(player.clone());
-        self.update_announcable_props(player.clone());
-        self.update_swap_trump(player.clone());
-        self.update_playable_cards(player.clone());
     }
 
     fn make_active(&mut self, player: Arc<RwLock<Player>>) -> Vec<tokio::task::JoinHandle<()>> {
